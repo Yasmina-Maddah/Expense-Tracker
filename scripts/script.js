a5dbc1c59c2e558a39bfeea37cb64cc10adfe641
Yasmina Maddah <maddahyasmina@gmail.com>
2024-11-04 23:41:39 +0200

    UnFinished problem

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,36 @@
-const transaction = { id: Date.now(), amount, type, date, notes };
+const transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+const budgetAmount = document.querySelector('.budget-amount');
+const transactionsList = document.querySelector('.transactions-list');
 
 function saveTransactions() {
-    localStorage.setItem('transactions', JSON.stringify(transaction));
+    localStorage.setItem('transactions', JSON.stringify(transactions));
 }
 
-function loadTransactions() {
-    return JSON.parse(localStorage.getItem('transaction')) || [];
+function calculateTotalBudget() {
+    const total = transactions.reduce((sum, transaction) => {
+      return transaction.type === 'income' ? sum + transaction.amount : sum - transaction.amount;
+    }, 0);
+    budgetAmount.textContent = total;
 }
 
-document.getElementById('transaction-form').addEventListener('submit', handleFormSubmit);
+document.querySelector('.transaction-form').addEventListener('submit', e => {
+    e.preventDefault();
+    const amount = parseFloat(document.querySelector('.amount').value);
+    const type = document.querySelector('.type').value;
+    const date = document.querySelector('.date').value;
 
+    const transaction = {amount, type, date };
+    transactions.push(transaction);
+    updateLocalStorage();
+    renderTransactions();
+    calculateTotalBudget();
+});
+
+document.querySelector('.filter-transactions').addEventListener('click', () => {
+    const min = parseFloat(document.querySelector('.filter-min').value) || null;
+    const max = parseFloat(document.querySelector('.filter-max').value) || null;
+    const type = document.querySelector('.filter-type').value;
+    const notes = document.querySelector('.filter-notes').value;
+});
+
+  
